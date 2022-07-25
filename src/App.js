4a586bcddd150e6a32fd53b4d66ba2fdccb491d5
Amna Jah <amnajah@Amnas-MacBook-Pro.local>
2022-07-25 16:53:29 +0500

    commit all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+
+  useEffect (()=> {
+    getName();
+  },[name]);
+
+  const getName = async() => {
+    const response = await axios.get("https://api.nationalize.io?name=nathaniel");
+    setName(response.data.name);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <p>
+        {name}
+      </p>
     </div>
   );
 }
-
 export default App;

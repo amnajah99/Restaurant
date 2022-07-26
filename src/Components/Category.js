694d2843556003b9377fe5c532f5733229fa4780
Amna Jah <amnajah@Amnas-MacBook-Pro.local>
2022-07-26 11:40:38 +0500

    Displaying list of categories in menu using map

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -2,13 +2,51 @@ import CategoryItem from './CategoryItem';
 import './Category.css';
 
 const Category = () => {
+    const menu_categories = [{
+        name: 'BBQ',
+    },
+    {
+        name: 'BURGER',
+    },
+    {
+        name: 'CHINESE',
+    },
+    {
+        name: 'DRINKS',
+    },
+    {
+        name: 'FRESH JUICE',
+    },
+    {
+        name: 'ICE CREAM',
+    },
+    {
+        name: 'MEATY',
+    },
+    {
+        name: 'PIZZA',
+    },
+    {
+        name: 'SALADS',
+    },
+    {
+        name: 'SEA FOOD',
+    },
+    {
+        name: 'SOUPS',
+    },
+    {
+        name: 'SWEETS',
+    },
+    ]
+    const cat_list = menu_categories.map((cat) => <CategoryItem name={cat.name} />)
     return (
         <div className="category">
             <div className="category-head">
                 <div className='category-title'>Categories</div>
                 <div className='category-count'>(12)</div>
             </div>
-            <CategoryItem />
+            {cat_list}
         </div>
     );
 }

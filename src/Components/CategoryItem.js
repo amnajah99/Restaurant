5490af6fe08601f6e830c228c229af7da4561513
Amna Jah <amnajah@Amnas-MacBook-Pro.local>
2022-07-26 11:30:34 +0500

    Modified style in category item

--- src/Components/CategoryItem.js
+++ src/Components/CategoryItem.js
@@ -1,9 +1,10 @@
 import chevron from '../images/chevron_right.svg';
+import './CategoryItem.css';
 
 const CategoryItem = () => {
     return (
         <div className='cat-item'>
-            <div className='cat-item-name'>Item name</div>
+            <div className='cat-item-name'>ITEM NAME</div>
             <div className='cat-item-arrow'><img src={chevron} /></div>
         </div>
     );

3118d18e5bc9d176ab207fcb7997fcc73666eb7e
Amna Jah <amnajah@Amnas-MacBook-Pro.local>
2022-07-26 16:16:58 +0500

    Styled menu card

--- src/Components/MenuCard.js
+++ src/Components/MenuCard.js
@@ -1,13 +1,15 @@
+import tick from '../images/tick.svg'
 import './MenuCard.css';
 
 const MenuCard = ({ name, price }) => {
     return (
         <div className="menu-card">
-            <div>{name}</div>
-            <div>
-                <div>{price}</div>
-                <div>1X</div>
+            <div className='menu-card-title'>{name}</div>
+            <div className='menu-card-details'>
+                <div className='menu-card-price'>Rs. {price}</div>
+                <div className='menu-card-amount'><img src={tick} /> 1X</div>
             </div>
+            <button className='add-to-cart-btn'>ADD TO CART</button>
         </div>
     );
 }

b788a5fe391751a15b8b168af53f9f2ffad6c04c
Amna Jah <amnajah@Amnas-MacBook-Pro.local>
2022-07-26 11:11:27 +0500

    Created category component for cuisine categories

---
+++ src/Components/Category.js
@@ -0,0 +1,13 @@
+import './Category.css';
+
+const Category = () => {
+    return (
+        <div className="category">
+            <div className="category-head">
+                <div className='category-title'>Categories</div>
+                <div className='category-count'>(12)</div>
+            </div>
+        </div>
+    );
+}
+export default Category;
